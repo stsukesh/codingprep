009d7570bd1af2950265c3b5c7e133cfc389655a
Sukesh S T <stsukesh@gmail.com>
2025-01-30 20:40:42 +0530

    prepend used

--- 290125/reverseString.java
+++ 290125/reverseString.java
@@ -3,16 +3,15 @@ import java.util.Scanner;
 public class reverseString {
     public static void main(String args[]){
         Scanner van = new Scanner(System.in);
-        
+        //i've done using prepend operation
         System.out.print("Enter a string: ");
         String n = van.nextLine();  // Input string
-        String s = "";  // Initialize s as an empty string
+        String s = "";  // Initialize s empty string
         char ch;
 
-        // Ensure we are looping within the bounds of the string length
         for (int i = 0; i < n.length(); i++) {
-            ch = n.charAt(i); // Get the character at the current index
-            s = ch + s;       // Prepend the character to the result string
+            ch = n.charAt(i); // Get the character
+            s = ch + s;       
         }
 
         System.out.println("Reversed string: " + s); // Print the reversed string
