4b46910a67e7bb09710333eb8b5791b258cf2e22
Sukesh S T <stsukesh@gmail.com>
2025-02-03 09:40:27 +0530

    retruns the 10's place of the number

---
+++ 030225/SecondLargestDigit.java
@@ -0,0 +1,9 @@
+public class SecondLargestDigit {
+          public static void main(String[] args) {
+                    int n=123;
+                    n/=10;
+                    int b=n%10;
+                    System.out.println(b);
+          }
+          
+}
