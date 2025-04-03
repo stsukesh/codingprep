6fe4f303d5b6b4a7b8ac0baed3b05484d9eca388
Sukesh S T <stsukesh@gmail.com>
2025-04-03 22:40:12 +0530

    1

---
+++ TeluskoJava/AnnonymousInnerClass.java
@@ -0,0 +1,24 @@
+public class AnnonymousInnerClass {
+          public static void main(String[] args){
+                    A obj= new A(){
+                              public void neaw(){
+                                        System.out.print("hello");
+                              }
+                    };
+                    obj.neaw();
+          }
+          
+}
+class A{
+          public void neaw(){}
+          public void show(){
+                    System.out.println("hello sukesh A");
+          }
+}
+class B extends A{
+          
+          public void show(){
+                    super.show();
+                    //System.out.println("hello sukesh B");
+          }
+}
