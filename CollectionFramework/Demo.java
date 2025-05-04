3156a6b6b6aa0c43980a7e1433def67aca14fc1c
Sukesh S T <stsukesh@gmail.com>
2025-05-04 22:53:59 +0330

    Update Demo.java

---
+++ CollectionFramework/Demo.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo{
+          public static void main(String[] args){
+                    List<String> s=new ArrayList<>();
+                    s.add("Sukesh");
+                    s.add("Sukesh");
+                    s.add("Sukesh");
+                    s.add("Sukesh");
+                    s.add("Su");
+                    s.add("Suk");
+                    System.out.println(s);
+                    
+
+          }
+}
