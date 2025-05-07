21fd6e360232e1146136221adf25a56919fda6bf
Sukesh S T <stsukesh@gmail.com>
2025-05-08 00:10:19 +0530

    Update AccessUpdate.java

---
+++ CollectionFramework/ArrayList/AccessUpdate.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccessUpdate {
+          public static void main(String[] args){
+                    List<String> fruits=new ArrayList<>(List.of("Mango","Organge","Pineapple"));
+                    System.out.println(fruits.size());
+
+
+                    System.out.println("First element: "+fruits.get(0));
+                    System.out.println("Last  element: "+fruits.get(fruits.size()-1));
+                    
+                    
+          }
+
+} 
